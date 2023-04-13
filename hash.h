81d55e8a1944438d12765eb30f7f6f44ea75c3d2
Jeremy Yiu <jeremyy@usc.edu>
2023-04-13 11:44:50 -0700

    Completed hash

--- hash.h
+++ hash.h
@@ -19,15 +19,39 @@ struct MyStringHash {
     // hash function entry point (i.e. this is h(k))
     HASH_INDEX_T operator()(const std::string& k) const
     {
-        // Add your code here
-
-
+        std::string copy = k;
+        size_t result = 0;
+        int counter = 4;
+        while(copy.size()> 6){
+            std::string substr = copy.substr(copy.size()-6);
+            int x = letterDigitToNumber(substr[0]);
+            for(int i = 1;i<6;i++){
+                x *= 36;
+                x += letterDigitToNumber(substr[i]);
+            }
+            std::cout << "w[] = " << rValues[counter]*x << std::endl;
+            result += rValues[counter]*x;
+            counter--;
+            copy = copy.substr(0,copy.size()-6);
+        }
+        int x = letterDigitToNumber(copy[0]);
+        for(int i = 1;i<copy.size();i++){
+            x *= 36;
+            x += letterDigitToNumber(copy[i]);
+        }
+        result += rValues[counter]*x;
+        return result;
     }
 
     // A likely helper function is to convert a-z,0-9 to an integral value 0-35
     HASH_INDEX_T letterDigitToNumber(char letter) const
     {
-        // Add code here or delete this helper function if you do not want it
+        if(letter < 60){return letter - '0' + 26;}
+        if(letter >= 'a'){
+            return letter - 'a';
+        }else{
+            return letter - 'A';
+        }
 
     }
 
